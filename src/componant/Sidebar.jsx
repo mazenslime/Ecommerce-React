1281cc56d701fe6265c9a4396dee82c7b28556df
Mazen slime <mazenslime16@gmail.com>
2026-08-28 15:23:04 +0300

    update fillter

--- src/componant/Sidebar.jsx
+++ src/componant/Sidebar.jsx
@@ -9,13 +9,19 @@ function Sidebar({openside,setopenside,Listcategoury,cat,setcat}) {
        <div className='w-full flrx flex-col p-4'>
          <h2 className='text-black mb-4 '>fillter by categoury</h2>
          <form>
+           <div className='flex items-center space-x-2 cursor-pointer '>
+                <input className='w-5 h-5 appearance-none checked:bg-[#C5A059] border-2 border-[#C5A059] rounded-lg ' type="radio" name='only' onChange={()=>{setcat(null)}}/>
+                <label htmlFor="">ALL</label>
+          </div>
          {
           Listcategoury.map((ele)=>{
             return(
+            <>
               <div className='flex items-center space-x-2 cursor-pointer '>
                 <input className='w-5 h-5 appearance-none checked:bg-[#C5A059] border-2 border-[#C5A059] rounded-lg ' type="radio" name='only' onChange={()=>{setcat(ele.id)}}/>
                 <label htmlFor="">{ele.slug}</label>
               </div>
+            </>
             )
           })
         }
